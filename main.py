d55c19c3c69fc3edd5d3950d717a9dd3ed3ae2e4
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-05-06 13:17:13 +0200

    Run multiple trials

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from typing import List
+from typing import List, Dict
 
 import torch
+import pandas as pd
+from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, random_split
 
 from lib.losses import HSIC
@@ -28,6 +32,7 @@ def experiment(
     cnn: bool = False,
     mlp_layers: List[int] = [],
     verbose: bool = True,
+    **kwargs,
 ):
     backbone = get_backbone(cnn=cnn, mlp_layers=mlp_layers)
     model = GenerativeFeatures(backbone, NUM_CLASSES)
@@ -52,7 +57,7 @@ def experiment(
     best_loss = 1e10
     for epoch_idx in range(num_epochs):
         if verbose:
-            print(f"Epoch {epoch_idx}")
+            print(f"\nEpoch {epoch_idx}")
         train_loss = train(
             model=model,
             criterion=hsic_one_hot,
@@ -81,16 +86,99 @@ def experiment(
     val_accuracy = compute_accuracy(model, val_dataloader)
     target_accuracy = compute_accuracy(model, target_dataloader)
 
-    print(f"{train_accuracy=}, {val_accuracy=}, {target_accuracy=}")
+    if verbose:
+        print(f"{train_accuracy=:.4f}, {val_accuracy=:.4f}, {target_accuracy=:.4f}\n")
+
+    results = {
+        "train_history": train_history,
+        "val_history": val_history,
+        "train_accuracy": train_accuracy,
+        "val_accuracy": val_accuracy,
+        "target_accuracy": target_accuracy,
+    }
+
+    return results
 
 
-def main(batch_size: int = 32, learning_rate: float = 1e-5, num_epochs: int = 100):
-    experiment(
-        batch_size=batch_size,
-        learning_rate=learning_rate,
-        num_epochs=num_epochs,
-        cnn=True,
-    )
+def multiple_trials(experiment_config: Dict, num_trials: int) -> Dict:
+    results = []
+    for i in tqdm(range(num_trials)):
+        trial_results = experiment(**experiment_config)
+        results.append(trial_results)
+
+    train_accuracy = [trial["train_accuracy"] for trial in results]
+    val_accuracy = [trial["val_accuracy"] for trial in results]
+    target_accuracy = [trial["target_accuracy"] for trial in results]
+
+    results = {
+        "train": pd.Series(train_accuracy).rename(experiment_config["model_name"]),
+        # "val": pd.Series(val_accuracy).rename(experiment_config["model_name"]),
+        "target": pd.Series(target_accuracy).rename(experiment_config["model_name"]),
+    }
+
+    return results
+
+
+def group_results(results: List[Dict]) -> pd.DataFrame:
+    keys = results[0].keys()
+
+    df_list = []
+    for key in keys:
+        df = pd.concat([exp_res[key] for exp_res in results], axis=1)
+        df = (
+            df.stack()
+            .rename("Accuracy")
+            .rename_axis(index=["exp", "model_name"])
+            .reset_index()
+        )
+        df["model_name"] = df["model_name"].apply(lambda x: x + f"_{key}")
+        df_list.append(df)
+    df = pd.concat(df_list)
+
+    return df
+
+
+def plot_results(df: pd.DataFrame, title: str = ""):
+    plt.ion()
+
+    ax = sns.boxplot(x="Accuracy", y="model_name", hue="loss_criterion", data=df)
+    ax.set(xscale="log")
+    ax.set_title(title)
+    ax.set_xscale("linear")
+
+
+def main(
+    num_trials: int = 1,  # 20,
+    num_epochs: int = 20,
+    batch_size: int = 32,
+    learning_rate: float = 1e-3,
+):
+    models = [
+        {"model_name": "CNN", "cnn": True},
+        # {"model_name": "MLP 2x256", "mlp_layers": [256, 256, 10]},
+        # {"model_name": "MLP 2x524", "mlp_layers": [524, 524, 10]},
+        # {"model_name": "MLP 2x1024", "mlp_layers": [1024, 1024, 10]},
+        # {"model_name": "MLP 4x256", "mlp_layers": [256, 256, 256, 256, 10]},
+        # {"model_name": "MLP 4x524", "mlp_layers": [524, 524, 524, 524, 10]},
+        # {"model_name": "MLP 4x1024", "mlp_layers": [1024, 1024, 1024, 1024, 10]},
+    ]
+
+    results = []
+    for model_config in models:
+        experiment_config = {
+            "num_epochs": num_epochs,
+            "batch_size": batch_size,
+            "learning_rate": learning_rate,
+        }
+        experiment_config = {**experiment_config, **model_config}
+        exp_results = multiple_trials(
+            num_trials=num_trials, experiment_config=experiment_config
+        )
+        results.append(exp_results)
+    results = group_results(results)
+    results["loss_criterion"] = "HSIC Classification"
+    plot_results(results)
+    results.to_csv("cls_results.csv", index=False)
 
 
 if __name__ == "__main__":
