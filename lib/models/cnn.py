d9df4117966c7de662b66c5e24631fe3f5c7a058
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-05-23 15:34:29 +0200

    Add back self.out_features attr

--- lib/models/cnn.py
+++ lib/models/cnn.py
@@ -6,13 +6,15 @@ class CNNBackbone(nn.Module):
     def __init__(self, in_channels: int = 1, out_features: int = 16):
         super().__init__()
 
+        self.out_features = out_features
+
         self.conv_1 = nn.Conv2d(
             in_channels=in_channels, out_channels=32, kernel_size=3, stride=1, padding=1
         )
         self.conv_2 = nn.Conv2d(
             in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1
         )
-        self.fc_1 = nn.Linear(in_features=7 * 7 * 64, out_features=out_features)
+        self.fc_1 = nn.Linear(in_features=7 * 7 * 64, out_features=self.out_features)
 
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
