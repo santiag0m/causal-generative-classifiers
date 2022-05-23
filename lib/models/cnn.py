66f9782dfc2d1e7732c46fca16ca900246567382
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-05-23 15:30:42 +0200

    Adjust cnn architecture

--- lib/models/cnn.py
+++ lib/models/cnn.py
@@ -3,21 +3,18 @@ import torch.nn as nn
 
 
 class CNNBackbone(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels: int = 1, out_features: int = 16):
         super().__init__()
 
-        self.out_features = 8
-
         self.conv_1 = nn.Conv2d(
-            in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1
+            in_channels=in_channels, out_channels=32, kernel_size=3, stride=1, padding=1
         )
         self.conv_2 = nn.Conv2d(
             in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1
         )
-        self.fc_1 = nn.Linear(in_features=7 * 7 * 64, out_features=self.out_features)
+        self.fc_1 = nn.Linear(in_features=7 * 7 * 64, out_features=out_features)
 
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.5)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
