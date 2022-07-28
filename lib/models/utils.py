63942c73f1c7abe9eec53da6328b66ca0300fdcf
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-07-29 00:57:25 +0200

    Fix names and dims

--- lib/models/utils.py
+++ lib/models/utils.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def multiply_prob_with_logits(logits_x, logits_y):
+def multiply_probs_with_logits(logits_x, logits_y):
     """
     sum = x + y
     z = - log(exp(-sum) + exp(-x) + exp(-y))
@@ -13,7 +13,7 @@ def multiply_prob_with_logits(logits_x, logits_y):
     return -1 * torch.logsumexp(terms, dim=-1, keepdim=False)
 
 
-def divide_prob_with_logits(logits_x, logits_y):
+def divide_probs_with_logits(logits_x, logits_y):
     """
     z = log(exp(0) + exp(-y)) - log(exp(-x) - exp(-y))
     sigmoid(z) = sigmoid(x) / sigmoid(y)
@@ -24,10 +24,10 @@ def divide_prob_with_logits(logits_x, logits_y):
 
     terms_right = -1 * torch.stack([logits_x, logits_y], dim=-1)
     # Do log sum exp trick
-    c = torch.max(terms_right, dim=-1, keepdim=False)
+    c = torch.max(terms_right, dim=-1, keepdim=True).values
     terms_right = terms_right - c
     terms_right = c + torch.log(
-        torch.exp(terms_right[..., [0]]) - torch.exp([..., [1]])
+        torch.exp(terms_right[..., 0]) - torch.exp([..., 1])
     )
 
     return terms_left - terms_right
