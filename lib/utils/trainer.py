5d8c3899ac984adc2afa677d5f0194451d5e9841
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-07-29 10:25:36 +0200

    Update training

--- lib/utils/trainer.py
+++ lib/utils/trainer.py
@@ -1,17 +1,17 @@
-from typing import Callable
+from typing import Callable, Optional
 
 import torch
 import torch.nn as nn
 from tqdm import tqdm
 from torch.utils.data import DataLoader
+from torch.nn.functional import cross_entropy
 
-from lib.losses.hsic import HSIC
-
+from lib.losses.hsic import hsic_one_hot
+from .running_average import RunningAverage
 
 def train(
     *,
     model: nn.Module,
-    criterion: Callable,
     dataloader: DataLoader,
     optim: torch.optim.Optimizer,
     use_pbar: bool = False,
@@ -24,14 +24,17 @@ def train(
         pbar = tqdm(enumerate(dataloader), total=len(dataloader))
     else:
         pbar = enumerate(dataloader)
+
+    accuracy = RunningAverage()
     for idx, (inputs, targets) in pbar:
         optim.zero_grad()
         inputs = inputs.to(device)
         targets = targets.to(device)
-        preds = model(inputs, targets)
-        loss = criterion(preds, targets)
+        residuals, logits = model(inputs, targets)
+        loss = hsic_one_hot(residuals, targets)
+        loss += cross_entropy(logits, targets)
 
-        mapped_feats = model.embedding_layer(targets)
+        mapped_feats = model.class_prototypes[targets, :]
 
         label_loss = 0
         num_feats = mapped_feats.shape[-1]
@@ -42,21 +45,23 @@ def train(
             )
 
         loss = loss + label_loss
-
         loss.backward()
         optim.step()
+
+        preds = torch.argmax(logits, dim=-1)
+        correct = logits == targets
+        accuracy.update(correct.cpu())
 
         cum_loss += loss.item()
         avg_loss = cum_loss / (idx + 1)
         if use_pbar:
-            pbar.set_description(f"Loss: {avg_loss}")
-    return avg_loss
+            pbar.set_description(f"Loss: {avg_loss:.4f} - Acc:{accuracy.value:.4f}")
+    return avg_loss, accuracy.value
 
 
 def eval(
     *,
     model: nn.Module,
-    criterion: Callable,
     dataloader: DataLoader,
     use_pbar: bool = False,
     num_classes: int = 10,
@@ -68,14 +73,17 @@ def eval(
         pbar = tqdm(enumerate(dataloader), total=len(dataloader))
     else:
         pbar = enumerate(dataloader)
+
     with torch.no_grad():
+        accuracy = RunningAverage()
         for idx, (inputs, targets) in pbar:
             inputs = inputs.to(device)
             targets = targets.to(device)
-            preds = model(inputs, targets)
-            loss = criterion(preds, targets)
+            residuals, logits = model(inputs, targets)
+            loss = hsic_one_hot(residuals, targets)
+            loss += cross_entropy(logits, targets)
 
-            mapped_feats = model.embedding_layer(targets)
+            mapped_feats = model.class_prototypes[targets, :]
 
             label_loss = 0
             num_feats = mapped_feats.shape[-1]
@@ -89,8 +97,12 @@ def eval(
 
             loss = loss + label_loss
 
+            preds = torch.argmax(logits, dim=-1)
+            correct = logits == targets
+            accuracy.update(correct.cpu())
+
             cum_loss += loss.item()
             avg_loss = cum_loss / (idx + 1)
             if use_pbar:
-                pbar.set_description(f"Loss: {avg_loss}")
-    return avg_loss
+                pbar.set_description(f"Loss: {avg_loss:.4f} - Acc:{accuracy.value:.4f}")
+    return avg_loss, accuracy.value
