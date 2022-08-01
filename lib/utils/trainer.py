5d9b40813c00497232278add41cd8d85db37a264
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-08-01 12:59:37 +0200

    Debug label loss

--- lib/utils/trainer.py
+++ lib/utils/trainer.py
@@ -66,7 +66,7 @@ def train(
             loss.backward()
             mmdm_optim.model_optim.step()
         else:
-            loss = mmdm_optim.lagrangian(main_loss=loss, constrained_loss=hsic_loss, target_value=0.3)
+            loss = mmdm_optim.lagrangian(main_loss=label_loss, constrained_loss=hsic_loss, target_value=0.3)
             loss.backward()
             mmdm_optim.step()
 
