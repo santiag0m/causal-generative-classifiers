4e0b69b63362677983b6cceb7ce78fbc733568be
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-08-01 14:06:48 +0200

    Don't optimize featurewise

--- lib/utils/trainer.py
+++ lib/utils/trainer.py
@@ -56,8 +56,8 @@ def train(
             hsic_loss = -1
             label_loss = -1
         else:
-            hsic_loss = hsic_residuals(residuals, targets)
-            label_loss = hsic_features(features, prototypes, targets)
+            hsic_loss = hsic_residuals(residuals, targets, featurewise=False)
+            label_loss = hsic_features(features, prototypes, targets, featurewise=False)
             # indep_loss = hsic_independence(residuals, targets)
             loss += label_loss # + indep_loss
             
