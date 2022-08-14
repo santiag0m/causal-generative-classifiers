3e20b5a7550e60bb0128030294d0dfeece2ad37c
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-08-14 12:29:37 +0200

    Use ELU + 1 instead of ReLU

--- lib/models/generative_features.py
+++ lib/models/generative_features.py
@@ -28,7 +28,6 @@ class GenerativeFeatures(nn.Module):
             *[self._spectral_norm(nn.Linear(self.hidden_dim, self.hidden_dim)),
             nn.ReLU()]*1,
             self._spectral_norm(nn.Linear(self.hidden_dim, 1)),
-            nn.ReLU(),  # Model outputs probability densities which cannot be negative
         )
         self.class_probs = nn.Parameter(
             torch.zeros((self.num_classes,)), requires_grad=False
@@ -51,10 +50,15 @@ class GenerativeFeatures(nn.Module):
 
     def classify_residuals(self, residuals: torch.Tensor) -> Tuple[torch.Tensor]:
         residuals = residuals.reshape(-1, self.hidden_dim)  # (Batch * Class, Features)
-        logits_z_y = torch.log(self.residual_classifier(residuals))  # (Batch * Class, 1)
+        logits_z_y = self._log_elu(self.residual_classifier(residuals))  # (Batch * Class, 1)
         logits_z_y = logits_z_y.reshape(-1, self.num_classes)  # (Batch, Class)
         logits_y_z = self.calculate_joint(logits_z_y)
         return logits_y_z
+
+    @staticmethod
+    def _log_elu(features: torch.Tensor) -> torch.Tensor:
+        log_features = torch.log(features + 1)
+        return torch.where(features >0, log_features, features)
 
 
     def calculate_joint(self, logits_z_y: torch.Tensor) -> torch.Tensor:
