9f06eb1f82295303c8f8d90032b5de9e07f8a92b
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-08-14 14:11:37 +0200

    epochs: 40

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ def plot_results(df: pd.DataFrame, title: str = ""):
 
 def main(
     num_trials: int = 20,
-    num_epochs: int = 30,
+    num_epochs: int = 40,
     batch_size: int = 32,
     learning_rate: float = 2e-2,
     spectral_norm: bool = False,
