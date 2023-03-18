b766a20bfef306e787e4352c61a32345fbe8d068
santiag0m <32421689+santiag0m@users.noreply.github.com>
2023-03-18 14:27:35 -0700

    HSIC baselines

--- experiments/hsic_experiment.py
+++ experiments/hsic_experiment.py
@@ -1,22 +1,37 @@
+import os
+import json
+import shutil
 from typing import List, Dict
 
 import torch
+from torchvision import transforms
 import pandas as pd
-from tqdm import tqdm
-import seaborn as sns
-import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, random_split
 
 from lib.losses import HSIC
-from lib.datasets import MNIST
 from lib.utils.trainer import train, eval
+from lib.datasets import ImbalancedImageFolder
 from lib.utils.accuracy import compute_accuracy
-from lib.models import get_backbone, CGCKDE
+from lib.models import get_backbone
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 NUM_CLASSES = 10
+
+TRIAL_FOLDER = "hsic_trial_results"
+
+def generate_class_unbalance():
+    ratios = torch.tensor([1] + [100] * 9, dtype=torch.float32)
+    weights = ratios / torch.sum(ratios)
+    return {str(i): weights[i] for i in range(NUM_CLASSES)}
+
+
+def generate_single_class():
+    ratios = torch.tensor([1] + [0] * 9, dtype=torch.float32)
+    weights = ratios / torch.sum(ratios)
+    return {str(i): weights[i] for i in range(NUM_CLASSES)}
+
 
 
 def hsic_one_hot(residuals: torch.tensor, targets: torch.tensor) -> torch.Tensor:
@@ -33,36 +48,77 @@ def hsic_one_hot(residuals: torch.tensor, targets: torch.tensor) -> torch.Tensor
 
 
 def experiment(
-    batch_size: int,
-    learning_rate: float,
-    num_epochs: int,
-    cnn: bool = False,
-    mlp_layers: List[int] = [],
+    hidden_dim: int = 10,
+    cifar10: bool = False,
+    seed: int = 0,
     verbose: bool = False,
     **kwargs,
 ):
-    backbone = get_backbone(cnn=cnn, mlp_layers=mlp_layers)
-    model = CGCKDE(backbone, NUM_CLASSES)
+    torch.manual_seed(seed)
+
+    if cifar10:
+        epochs = 50
+        learning_rate = 0.01
+        batch_size = 128
+        weight_decay = 0.0001
+        momentum = 0.9
+        model_name = "cifar10"
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+            ]
+        )
+    else:
+        epochs = 20
+        learning_rate = 5e-2
+        batch_size = 32
+        weight_decay = 0
+        momentum = 0
+        model_name = "mnist"
+        transform = transforms.Compose(
+            [
+                transforms.Grayscale(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+    model = get_backbone(model_name=model_name, hidden_dim=hidden_dim)
     model.to(DEVICE)
 
     # Create Datasets
-    source_dataset = MNIST(rotated=False, train=True)
+    source_dataset = ImbalancedImageFolder(
+        f"data/class_{model_name}/train",
+        class_weights=generate_class_unbalance(),
+        seed=seed,
+        transform=transform,
+    )
     train_dataset, val_dataset, _ = random_split(
         source_dataset, [10_000, 1_000, len(source_dataset) - 11_000]
     )
-    target_dataset = MNIST(rotated=True, train=False)
+    target_dataset = ImbalancedImageFolder(
+        f"data/class_{model_name}/test",
+        class_weights=generate_single_class(),
+        seed=seed,
+        transform=transform,
+    )
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
     target_dataloader = DataLoader(target_dataset, batch_size=batch_size)
 
     # Setup Optimizer
-    optim = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
+    optim = torch.optim.SGD(
+                model.parameters(),
+                lr=learning_rate,
+                momentum=momentum,
+                weight_decay=weight_decay,
+            ),
 
     # Train
     train_history = []
     val_history = []
     best_loss = 1e10
-    for epoch_idx in range(num_epochs):
+    for epoch_idx in range(epochs):
         if verbose:
             print(f"\nEpoch {epoch_idx}")
         train_loss = train(
@@ -85,10 +141,8 @@ def experiment(
             torch.save(model.state_dict(), "./best.pth")
             best_loss = val_loss
 
-    # Fit KDE after training
-    model.fit_kde(train_dataloader, verbose=verbose)
-
     # Check accuracy
+    model.predict = model.forward
     train_accuracy = compute_accuracy(model, train_dataloader)
     val_accuracy = compute_accuracy(model, val_dataloader)
     target_accuracy = compute_accuracy(model, target_dataloader)
@@ -107,20 +161,32 @@ def experiment(
 
 
 def multiple_trials(experiment_config: Dict, num_trials: int) -> Dict:
+    if os.path.isdir(TRIAL_FOLDER):
+        shutil.rmtree(TRIAL_FOLDER)
+    os.makedirs(TRIAL_FOLDER)
+
     results = []
-    for i in tqdm(range(num_trials)):
-        print(f"Trial {i+1}/{num_trials}")
-        trial_results = experiment(**experiment_config)
+    for i in range(num_trials):
+        print(f"Experiment {i+1}/{num_trials}")
+        trial_results = experiment(**experiment_config, seed=i)
         results.append(trial_results)
+        with open(os.path.join(TRIAL_FOLDER, f"trial_{i:03d}.json"), "w") as f:
+            json.dump(trial_results, f)
 
     train_accuracy = [trial["train_accuracy"] for trial in results]
     val_accuracy = [trial["val_accuracy"] for trial in results]
     target_accuracy = [trial["target_accuracy"] for trial in results]
 
     results = {
-        "train": pd.Series(train_accuracy).rename(experiment_config["model_name"]),
-        # "val": pd.Series(val_accuracy).rename(experiment_config["model_name"]),
-        "target": pd.Series(target_accuracy).rename(experiment_config["model_name"]),
+        "train": pd.Series(train_accuracy)
+        .fillna(0)
+        .rename(experiment_config["model_name"]),
+        "val": pd.Series(val_accuracy)
+        .fillna(0)
+        .rename(experiment_config["model_name"]),
+        "target": pd.Series(target_accuracy)
+        .fillna(0)
+        .rename(experiment_config["model_name"]),
     }
 
     return results
@@ -138,44 +204,29 @@ def group_results(results: List[Dict]) -> pd.DataFrame:
             .rename_axis(index=["exp", "model_name"])
             .reset_index()
         )
-        df["model_name"] = df["model_name"].apply(lambda x: x + f"_{key}")
+        df["setting"] = key
         df_list.append(df)
     df = pd.concat(df_list)
 
     return df
 
 
-def plot_results(df: pd.DataFrame, title: str = ""):
-    plt.ion()
-
-    ax = sns.boxplot(x="Accuracy", y="model_name", hue="loss_criterion", data=df)
-    ax.set(xscale="log")
-    ax.set_title(title)
-    ax.set_xscale("linear")
-
-
 def main(
-    num_trials: int = 7,
-    num_epochs: int = 7,
-    batch_size: int = 32,
-    learning_rate: float = 1e-3,
+    cifar10: bool = False,
+    hidden_dim: int = 10,
+    num_trials: int = 20,
+    use_softmax: bool = False,
 ):
     models = [
         {"model_name": "CNN", "cnn": True},
-        # {"model_name": "MLP 2x256", "mlp_layers": [256, 256, 10]},
-        # {"model_name": "MLP 2x524", "mlp_layers": [524, 524, 10]},
-        # {"model_name": "MLP 2x1024", "mlp_layers": [1024, 1024, 10]},
-        # {"model_name": "MLP 4x256", "mlp_layers": [256, 256, 256, 256, 10]},
-        # {"model_name": "MLP 4x524", "mlp_layers": [524, 524, 524, 524, 10]},
-        # {"model_name": "MLP 4x1024", "mlp_layers": [1024, 1024, 1024, 1024, 10]},
     ]
 
     results = []
     for model_config in models:
         experiment_config = {
-            "num_epochs": num_epochs,
-            "batch_size": batch_size,
-            "learning_rate": learning_rate,
+            "cifar10": cifar10,
+            "hidden_dim": hidden_dim,
+            "use_softmax": use_softmax
         }
         experiment_config = {**experiment_config, **model_config}
         exp_results = multiple_trials(
@@ -183,10 +234,13 @@ def main(
         )
         results.append(exp_results)
     results = group_results(results)
-    results["loss_criterion"] = "HSIC Classification"
-    plot_results(results)
-    results.to_csv("cls_results.csv", index=False)
+    results.to_csv("hsic_results.csv", index=False)
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Experiment setup")
+    parser.add_argument("--use_softmax", action="store_true")
+    args = parser.parse_args()
+    main(use_softmax=args.use_softmax)
