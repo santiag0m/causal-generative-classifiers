01bde3e6fa80b1c91c5a775a9e93f502293e2efa
santiag0m <32421689+santiag0m@users.noreply.github.com>
2023-05-16 08:39:38 -0700

    Generalize mmdm trainer

--- lib/utils/mmdm_trainer.py
+++ lib/utils/mmdm_trainer.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional
+from typing import Optional
 
 import torch
 import torch.nn as nn
@@ -6,18 +6,19 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 from torch.nn.functional import cross_entropy
 
+from lib.models import CGCResidual
+from lib.losses.hsic import hsic_residuals
 from .mmdm import MMDMOptim
 from .running_average import RunningAverage
-from lib.losses.hsic import hsic_residuals
-
 
 def train(
     *,
     model: nn.Module,
     dataloader: DataLoader,
-    mmdm_optim: MMDMOptim,
+    optim: MMDMOptim,
     use_pbar: bool = False,
     use_hsic: bool = True,
+    class_weights: Optional[torch.Tensor] = None,
 ) -> float:
     model.train()
     device = next(model.parameters()).device
@@ -31,41 +32,43 @@ def train(
 
     accuracy = RunningAverage()
     for idx, (inputs, targets) in pbar:
-        mmdm_optim.zero_grad()
+        optim.zero_grad()
         inputs = inputs.to(device)
         targets = targets.to(device)
 
-        features = model.get_features(inputs)
-        residuals = model.get_residuals(features)
-        logits = model.classify_residuals(residuals)
-
-        ce_loss = cross_entropy(logits, targets)
-
         if use_hsic:
+            if not isinstance(model, CGCResidual):
+                raise TypeError(f"Expected model of class 'CGCResidual', got '{type(model)}'")
+            
+            z = model.get_features(inputs)
+            residuals = model.get_residuals(z)
+            logits = model.classify_residuals(residuals)
+
+            # MMDM Update
             hsic_loss = hsic_residuals(residuals, targets, featurewise=False)
-        else:
-            hsic_loss = hsic_residuals(residuals, targets, featurewise=False).item()
-
-        if use_hsic:
-            loss = mmdm_optim.lagrangian(
+            ce_loss = cross_entropy(logits, targets, weight=class_weights)
+            loss = optim.lagrangian(
                 main_loss=ce_loss, constrained_loss=hsic_loss, target_value=0
             )
             loss.backward()
-            mmdm_optim.step()
+            optim.step()
 
             hsic_loss = hsic_loss.item()
-            ce_loss = ce_loss.item()
+            cum_hsic_loss += hsic_loss
         else:
+            # Normal update
+            logits = model(inputs)
+            ce_loss = cross_entropy(logits, targets, weight=class_weights)
             ce_loss.backward()
-            mmdm_optim.model_optim.step()
-            ce_loss = ce_loss.item()
+            optim.model_optim.step()
+        
+        ce_loss = ce_loss.item()
 
         preds = torch.argmax(logits, dim=-1)
         correct = preds == targets
         accuracy.update(correct.cpu())
         avg_acc = accuracy.value
 
-        cum_hsic_loss += hsic_loss
         cum_ce_loss += ce_loss
 
         avg_hsic_loss = cum_hsic_loss / (idx + 1)
@@ -83,7 +86,7 @@ def eval(
     model: nn.Module,
     dataloader: DataLoader,
     use_pbar: bool = False,
-    use_hsic: bool = True,
+    return_confusion_matrix: bool = False,
 ) -> float:
     model.eval()
     device = next(model.parameters()).device
@@ -97,18 +100,22 @@ def eval(
 
     with torch.no_grad():
         accuracy = RunningAverage()
+
+        confusion_matrix = 0
+
         for idx, (inputs, targets) in pbar:
             inputs = inputs.to(device)
             targets = targets.to(device)
 
-            features = model.get_features(inputs)
-            residuals = model.get_residuals(features)
-            logits = model.classify_residuals(residuals)
+            if isinstance(model, CGCResidual):
+                z = model.get_features(inputs)
+                residuals = model.get_residuals(z)
+                logits = model.classify_residuals(residuals)
 
-            if use_hsic:
-                hsic_loss = hsic_residuals(residuals, targets).item()
+                hsic_loss = hsic_residuals(residuals, targets, featurewise=False).item()
+                cum_hsic_loss += hsic_loss
             else:
-                hsic_loss = -1
+                logits = model(inputs)
 
             ce_loss = cross_entropy(logits, targets).item()
 
@@ -117,9 +124,16 @@ def eval(
             accuracy.update(correct.cpu())
             avg_acc = accuracy.value
 
-            cum_hsic_loss += hsic_loss
-            cum_ce_loss += ce_loss
+            if return_confusion_matrix:
+                batch_confusion_matrix = torch.sparse_coo_tensor(
+                    indices=torch.stack([preds, targets], dim=1).T,
+                    values=torch.ones(inputs.shape[0], device=preds.device),
+                    size=(logits.shape[-1], logits.shape[-1]),
+                )
+                batch_confusion_matrix = batch_confusion_matrix.coalesce().to_dense()
+                confusion_matrix += batch_confusion_matrix
 
+            cum_ce_loss += ce_loss
             avg_hsic_loss = cum_hsic_loss / (idx + 1)
             avg_ce_loss = cum_ce_loss / (idx + 1)
 
@@ -127,4 +141,8 @@ def eval(
                 pbar.set_description(
                     f"[VAL] {avg_hsic_loss=:.3f} {avg_ce_loss=:.3f} {avg_acc=:.3f}"
                 )
-    return avg_hsic_loss, avg_ce_loss, avg_acc.item()
+
+    if return_confusion_matrix:
+        return avg_hsic_loss, avg_ce_loss, avg_acc.item(), confusion_matrix
+    else:
+        return avg_hsic_loss, avg_ce_loss, avg_acc.item()
